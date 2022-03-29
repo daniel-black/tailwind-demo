d7de52ea322b50ac13d92db61c132834c9d9f0ba
Dan <drblack651@gmail.com>
2022-03-28 23:56:27 -0500

    decent looking header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="bg-sky-500 text-sky-100 text-6xl flex flex-row justify-between font-extralight items-center shadow-md shadow-sky-600">
+    <h2 class="ml-5 hover:text-white py-3">
+      Campify
+    </h2>
+    <div class="text-3xl mr-5">
+      <a class="transform ml-8 py-2 px-4 hover:bg-sky-600 hover:text-white hover:shadow-inner transition duration-150 rounded-full " href="#">
+         About
+      </a>
+      <a class="transform ml-8 py-2 px-4 hover:bg-sky-600 hover:text-white hover:shadow-inner transition duration-150 rounded-full  " href="#">Search</a>
+      <a class="transform ml-8 py-2 px-4 hover:bg-sky-600 hover:text-white hover:shadow-inner transition duration-150 rounded-full  " href="#">Login</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
